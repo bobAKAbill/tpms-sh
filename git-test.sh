fd8f5f00733ee2ce089a26dce4913ffcab7f14ff
bobAKAbill <bmwalker@scu.edu>
2019-08-12 14:09:35 -0400

    Added git-test.sh
    Added actual shell script

---
+++ git-test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#/This script should be run by typing '. ./git-test.sh'
+
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo git clone https://github.com/merbanan/rtl_433.git
+cd rtl_433
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
+sudo apt install mosquitto-clients
