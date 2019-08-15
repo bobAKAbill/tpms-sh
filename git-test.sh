486a4fffecfb0dbdaa5a92048c3d5fceec9582b1
Brian Walker <bmwalker@scu.edu>
2019-08-15 15:02:17 -0400

    Update git-test.sh
    Added "sudo apt install cmake" to ensure that cmake was installed

--- git-test.sh
+++ git-test.sh
@@ -5,11 +5,11 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt install cmake
 
-sudo git clone https://github.com/merbanan/rtl_433.git
+sudo git clone https://github.com/merbanan/rtl_433.git  #Clones the git repository the sniffer program
 cd rtl_433
 mkdir build
 cd build
-cmake ..
-make
+cmake ..                                                #Makes files in the above directory
+make                                                    #Makes files in current directory
 sudo make install
 sudo apt install mosquitto-clients
